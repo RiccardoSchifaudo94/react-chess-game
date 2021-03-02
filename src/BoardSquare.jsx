376379ce757fe08ac43a2d51fdbd2c80223ff869
Ricky <mr.emps94@gmail.com>
2021-03-02 23:15:21 +0100

    finish chess

--- src/BoardSquare.jsx
+++ src/BoardSquare.jsx
@@ -25,7 +25,7 @@ export default function BoardSquare({ piece , black, position }) {
 
       )
       return () => subscribe.unsubscribe(); 
-    }, [])
+    }, [position])
     return (
       <div className="board-square" ref={drop}>
           <Square black={black}>
